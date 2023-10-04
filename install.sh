138cdf87287b6740e64e225a50f1982bfdbc4090
zgsec <78429972+zgsec@users.noreply.github.com>
2023-10-03 22:22:33 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Make the scripts executable
+chmod +x BLEScanner.py
+chmod +x BLEEnumerator.py
+
+# Install bluepy using pip3
+echo "Installing bluepy..."
+pip3 install bluepy
+
+echo "Installation complete."
